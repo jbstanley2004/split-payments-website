de5a16d0b648c981b110f0e66507a64589eee6a5
jbstanley2004 <131924947+jbstanley2004@users.noreply.github.com>
2025-11-13 02:55:00 -0500

    Remove footer CardBeam animation component

--- src/components/FooterBeam.tsx
+++ src/components/FooterBeam.tsx
@@ -1,14 +1,3 @@
-"use client";
-import dynamic from 'next/dynamic';
-
-const CardBeam = dynamic(() => import('@/components/CardBeamAnimation'), { ssr: false });
-
 export default function FooterBeam() {
-  return (
-    <div aria-hidden className="mt-24">
-      <div className="w-screen relative left-1/2 right-1/2 -mx-[50vw]">
-        <CardBeam />
-      </div>
-    </div>
-  );
+  return null;
 }
