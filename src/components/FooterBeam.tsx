c74366fb2fa7d7fd8f1bddfdb160320799679f6c
jbstanley2004 <131924947+jbstanley2004@users.noreply.github.com>
2025-11-13 02:06:13 -0500

    Restyle footer CardBeam wrapper for full-width edge-to-edge footer animation

--- src/components/FooterBeam.tsx
+++ src/components/FooterBeam.tsx
@@ -1,10 +1,14 @@
 "use client";
 import dynamic from 'next/dynamic';
+
 const CardBeam = dynamic(() => import('@/components/CardBeamAnimation'), { ssr: false });
+
 export default function FooterBeam() {
   return (
     <div aria-hidden className="mt-24">
-      <CardBeam />
+      <div className="w-screen relative left-1/2 right-1/2 -mx-[50vw]">
+        <CardBeam />
+      </div>
     </div>
   );
 }
