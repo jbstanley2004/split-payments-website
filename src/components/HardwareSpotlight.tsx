80d99eed40240bbf33c0e049c8c0ab5702c88206
jbstanley2004 <jbstanley2004@gmail.com>
2025-11-20 02:00:48 -0500

    fix: update HardwareSpotlight to use dynamic product data

--- src/components/HardwareSpotlight.tsx
+++ src/components/HardwareSpotlight.tsx
@@ -1,420 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
-
-// This data is a result of merging the spec file with the image URL file.
-// Only items with a confident image URL match have been included.
-const hardwareData = [
-  {
-    "image": "https://www.cardmachineoutlet.com/1_778b0f4c-bb86-4496-be87-5744307812c0_grande.png",
-    "model": "Ingenico Desk 3500 Dial/Ethernet (PCD30012573S)",
-    "brand": "Ingenico",
-    "category": "Terminal",
-    "features": {
-      "Ethernet": "Yes"
-    },
-    "specs": {
-      "Ethernet": "Yes"
-    }
-  },
-  {
-    "image": "https://www.cardmachineoutlet.com/1_4757932c-252f-43c4-a1eb-73d7c7903cdc_grande.png",
-    "model": "Ingenico Lane 5000 PIN Pad (PRB30311574I)",
-    "brand": "Ingenico",
-    "category": "PIN Pad",
-    "features": {
-      "EMV Chip": "Yes"
-    },
-    "specs": {
-      "EMV Chip": "Yes"
-    }
-  },
-  {
-    "image": "https://www.cardmachineoutlet.com/1_f0a36db0-2bfa-4e13-b1a2-bc9927dadc43_grande.png",
-    "model": "Ingenico Lane 7000 PIN Pad (PTT30311577I)",
-    "brand": "Ingenico",
-    "category": "PIN Pad",
-    "features": {
-      "EMV Chip": "Yes"
-    },
-    "specs": {
-      "EMV Chip": "Yes"
-    }
-  },
-  {
-    "image": "https://www.cardmachineoutlet.com/1_f18bb4b0-1f35-4704-b756-3321586448a3_grande.png",
-    "model": "Ingenico Lane 8000 PIN Pad (PTT30311578I)",
-    "brand": "Ingenico",
-    "category": "PIN Pad",
-    "features": {
-      "EMV Chip": "Yes"
-    },
-    "specs": {
-      "EMV Chip": "Yes"
-    }
-  },
-  {
-    "image": "https://www.cardmachineoutlet.com/1_54a3577a-ef9c-49de-8b04-4beb35d8ca1a_grande.png",
-    "model": "Ingenico Link 2500 Wi-Fi / Bluetooth (PWR2132002A)",
-    "brand": "Ingenico",
-    "category": "Mobile / Handheld",
-    "features": {
-      "Wi-Fi": "Yes",
-      "Bluetooth": "Yes"
-    },
-    "specs": {
-      "Wi-Fi": "Yes",
-      "Bluetooth": "Yes"
-    }
-  },
-  {
-    "image": "https://www.cardmachineoutlet.com/1_6e61f0bf-9357-4f04-8fa9-56bd4d7a115a_grande.png",
-    "model": "Ingenico Move 5000 4G Wi-Fi Bluetooth (PND205111702742A)",
-    "brand": "Ingenico",
-    "category": "Mobile / Handheld",
-    "features": {
-      "Wi-Fi": "Yes",
-      "Cellular": "4G",
-      "Bluetooth": "Yes"
-    },
-    "specs": {
-      "Wi-Fi": "Yes",
-      "Cellular": "4G",
-      "Bluetooth": "Yes"
-    }
-  },
-  {
-    "image": "https://www.cardmachineoutlet.com/1_5e4a3d8d-1044-4f36-a8b0-9652be09b8f0_grande.png",
-    "model": "Ingenico Move 5000 Wi-Fi / Bluetooth (PND205111702721A)",
-    "brand": "Ingenico",
-    "category": "Mobile / Handheld",
-    "features": {
-      "Wi-Fi": "Yes",
-      "Bluetooth": "Yes"
-    },
-    "specs": {
-      "Wi-Fi": "Yes",
-      "Bluetooth": "Yes"
-    }
-  },
-  {
-    "image": "https://www.cardmachineoutlet.com/1_4b91161e-3207-4e23-ab42-1550c9a0e53a_grande.png",
-    "model": "Ingenico Move 5000 w/ Charging Base (PND205111702721A + PWR201520A)",
-    "brand": "Ingenico",
-    "category": "Mobile / Handheld",
-    "features": {
-      "Bluetooth": "Yes"
-    },
-    "specs": {
-      "Bluetooth": "Yes"
-    }
-  },
-  {
-    "image": "https://www.cardmachineoutlet.com/1_0c841fe8-1fd7-4987-8a31-0a98ee8b6b74_grande.png",
-    "model": "Ingenico Desk 5000 Ethernet / Dial (BOA201139873)",
-    "brand": "Ingenico",
-    "category": "Countertop",
-    "features": {
-      "Ethernet": "Yes"
-    },
-    "specs": {
-      "Ethernet": "Yes"
-    }
-  },
-  {
-    "image": "https://www.cardmachineoutlet.com/1_9d4f7d15-2a0c-48b1-ad2c-5a7fef6a3bd4_grande.png",
-    "model": "Ingenico Desk 5000 Base Station (PWR201520A)",
-    "brand": "Ingenico",
-    "category": "Countertop"
-  },
-  {
-    "image": "https://www.cardmachineoutlet.com/1_b2ec46e2-31d1-44a7-a7b6-9c92fdb3b83a_grande.png",
-    "model": "Ingenico iPP320 PIN Pad (PNI2152201R)",
-    "brand": "Ingenico",
-    "category": "PIN Pad"
-  },
-  {
-    "image": "https://www.cardmachineoutlet.com/1_6cf94de5-bb48-46dd-a494-2e5e1b3594d1_grande.png",
-    "model": "Ingenico iPP350 PIN Pad (PNI2152202R)",
-    "brand": "Ingenico",
-    "category": "PIN Pad"
-  },
-  {
-    "image": "https://www.cardmachineoutlet.com/1_a5cf0fda-33a0-403e-9b2c-cbcbd9722837_grande.png",
-    "model": "Verifone VX520 Dial/Ethernet (M252-653-A3-NAA-3)",
-    "brand": "Verifone",
-    "category": "Countertop",
-    "features": {
-      "Ethernet": "Yes"
-    },
-    "specs": {
-      "Ethernet": "Yes"
-    }
-  },
-  {
-    "image": "https://www.cardmachineoutlet.com/1_2d7d0b17-ffb7-425e-bf74-8e1d1d7745a9_grande.png",
-    "model": "Verifone P200 PIN Pad (M435-003-04-NAA-3)",
-    "brand": "Verifone",
-    "category": "PIN Pad",
-    "features": {
-      "EMV Chip": "Yes"
-    },
-    "specs": {
-      "EMV Chip": "Yes"
-    }
-  },
-  {
-    "image": "https://www.cardmachineoutlet.com/1_a1c1a2b0-6f1c-4e24-b2d6-fd997b05e06f_grande.png",
-    "model": "Verifone P400 PIN Pad (M435-001-04-NAA-5)",
-    "brand": "Verifone",
-    "category": "PIN Pad",
-    "features": {
-      "EMV Chip": "Yes"
-    },
-    "specs": {
-      "EMV Chip": "Yes"
-    }
-  },
-  {
-    "image": "https://www.cardmachineoutlet.com/1_1d2fb7da-3b0a-4f7f-8b1a-f1d9a7d68a1f_grande.png",
-    "model": "Verifone M400 PIN Pad (M400-403-01-R)",
-    "brand": "Verifone",
-    "category": "PIN Pad",
-    "features": {
-      "EMV Chip": "Yes"
-    },
-    "specs": {
-      "EMV Chip": "Yes"
-    }
-  },
-  {
-    "image": "https://www.cardmachineoutlet.com/1_1a4b6b92-6f14-4b91-9c44-9d3b5a29d2c1_grande.png",
-    "model": "Verifone M440 PIN Pad (M440-403-01-R)",
-    "brand": "Verifone",
-    "category": "PIN Pad",
-    "features": {
-      "EMV Chip": "Yes"
-    },
-    "specs": {
-      "EMV Chip": "Yes"
-    }
-  },
-  {
-    "image": "https://www.cardmachineoutlet.com/1_47a29d9f-1c48-4a70-84a6-4d5fbe8a0f9e_grande.png",
-    "model": "Verifone VX805 PIN Pad (M132-409-01-R)",
-    "brand": "Verifone",
-    "category": "PIN Pad"
-  },
-  {
-    "image": "https://www.cardmachineoutlet.com/1_d0a3fd45-8a90-4a47-9c83-67a7e9e7a6f1_grande.png",
-    "model": "PAX A80 Ethernet / Wi-Fi / Dial (A80-0W0-363-01EA)",
-    "brand": "PAX",
-    "category": "Countertop",
-    "features": {
-      "Wi-Fi": "Yes",
-      "Ethernet": "Yes"
-    },
-    "specs": {
-      "Wi-Fi": "Yes",
-      "Ethernet": "Yes"
-    }
-  },
-  {
-    "image": "https://www.cardmachineoutlet.com/1_3f0d02f3-02e5-4a72-a5f1-0435f44e1ef6_grande.png",
-    "model": "PAX A920 Pro 4G / Wi-Fi / Bluetooth (A920Pro-0W0-363-01EA)",
-    "brand": "PAX",
-    "category": "Mobile / Handheld",
-    "features": {
-      "Wi-Fi": "Yes",
-      "Cellular": "4G",
-      "Bluetooth": "Yes"
-    },
-    "specs": {
-      "Wi-Fi": "Yes",
-      "Cellular": "4G",
-      "Bluetooth": "Yes"
-    }
-  },
-  {
-    "image": "https://www.cardmachineoutlet.com/1_0a2dd7e3-1b01-4476-a47b-569b14740aa4_grande.png",
-    "model": "PAX A35 PIN Pad (A35-0W0-363-01EA)",
-    "brand": "PAX",
-    "category": "PIN Pad",
-    "features": {
-      "EMV Chip": "Yes"
-    },
-    "specs": {
-      "EMV Chip": "Yes"
-    }
-  },
-  {
-    "image": "https://www.cardmachineoutlet.com/1_2b28fb1f-2a5e-4107-8406-19ae2b3f6e61_grande.png",
-    "model": "PAX A77 4G / Wi-Fi / Bluetooth (A77-0W0-363-01EA)",
-    "brand": "PAX",
-    "category": "Mobile / Handheld",
-    "features": {
-      "Wi-Fi": "Yes",
-      "Cellular": "4G",
-      "Bluetooth": "Yes"
-    },
-    "specs": {
-      "Wi-Fi": "Yes",
-      "Cellular": "4G",
-      "Bluetooth": "Yes"
-    }
-  },
-  {
-    "image": "https://www.cardmachineoutlet.com/1_62d5f2e1-f4f3-4c9f-8b4e-5928b9b3c3a8_grande.png",
-    "model": "PAX A920 4G / Wi-Fi / Bluetooth (A920-0W0-363-01EA)",
-    "brand": "PAX",
-    "category": "Mobile / Handheld",
-    "features": {
-      "Wi-Fi": "Yes",
-      "Cellular": "4G",
-      "Bluetooth": "Yes"
-    },
-    "specs": {
-      "Wi-Fi": "Yes",
-      "Cellular": "4G",
-      "Bluetooth": "Yes"
-    }
-  },
-  {
-    "image": "https://www.cardmachineoutlet.com/1_6b9a6a90-0f8f-4e6e-9d3b-3a3c85bb1a32_grande.png",
-    "model": "PAX E700 All-in-One (E700-0W0-363-01EA)",
-    "brand": "PAX",
-    "category": "All-in-One / ECR"
-  },
-  {
-    "image": "https://www.cardmachineoutlet.com/1_3a2de478-3a5b-4a52-af1a-1a2a75949bdb_grande.png",
-    "model": "PAX E800 All-in-One (E800-0W0-363-01EA)",
-    "brand": "PAX",
-    "category": "All-in-One / ECR"
-  },
-  {
-    "image": "https://www.cardmachineoutlet.com/1_2f1b3a9f-af8e-4a3d-8137-03a7ac2a3d5d_grande.png",
-    "model": "Dejavoo Z11 Wi-Fi / Ethernet (Z11-WIFI-ETH)",
-    "brand": "Dejavoo",
-    "category": "Countertop",
-    "features": {
-      "Wi-Fi": "Yes",
-      "Ethernet": "Yes"
-    },
-    "specs": {
-      "Wi-Fi": "Yes",
-      "Ethernet": "Yes"
-    }
-  },
-  {
-    "image": "https://www.cardmachineoutlet.com/1_4e21a3b4-21bc-4e3f-97f9-1e7d9e19d77d_grande.png",
-    "model": "Dejavoo Z8 Dial / Ethernet (Z8-ETH-DIAL)",
-    "brand": "Dejavoo",
-    "category": "Countertop",
-    "features": {
-      "Ethernet": "Yes"
-    },
-    "specs": {
-      "Ethernet": "Yes"
-    }
-  },
-  {
-    "image": "https://www.cardmachineoutlet.com/1_1ad81e3e-2f61-4ec0-9a2f-bcb1ea0a1b6a_grande.png",
-    "model": "Castles MP200 PIN Pad",
-    "brand": "Castles",
-    "category": "PIN Pad"
-  },
-  {
-    "image": "https://www.cardmachineoutlet.com/1_8b4c58e1-7b88-4479-b94d-fbfb1a4d3a5f_grande.png",
-    "model": "Castles Vega 3000 Desktop",
-    "brand": "Castles",
-    "category": "Countertop"
-  },
-  {
-    "image": "https://www.cardmachineoutlet.com/1_2e3755a2-4b4a-4b5d-8f2b-5cbf0e8e5b9c_grande.png",
-    "model": "BBPOS Chipper BT",
-    "brand": "BBPOS",
-    "category": "Mobile / Handheld",
-    "features": {
-      "Bluetooth": "Yes"
-    },
-    "specs": {
-      "Bluetooth": "Yes"
-    }
-  },
-  {
-    "image": "https://www.cardmachineoutlet.com/1_1b7b6087-b2f7-4cda-9dae-1e2a7e3e2a5f_grande.png",
-    "model": "Nexgo G2 Countertop",
-    "brand": "Nexgo",
-    "category": "Countertop"
-  },
-  {
-    "image": "https://www.cardmachineoutlet.com/1_6d5b7a2f-26a0-43cf-9e0a-3408f3e1b9e2_grande.png",
-    "model": "Poynt 5 Android Wi-Fi / Bluetooth",
-    "brand": "Poynt",
-    "category": "Mobile / Handheld",
-    "features": {
-      "Wi-Fi": "Yes",
-      "Bluetooth": "Yes"
-    },
-    "specs": {
-      "Wi-Fi": "Yes",
-      "Bluetooth": "Yes"
-    }
-  },
-  {
-    "image": "https://www.cardmachineoutlet.com/1_7f2c6b1a-2f69-4c93-9f86-7a2b3f2e5a1d_grande.png",
-    "model": "Clover Flex Wi-Fi / LTE",
-    "brand": "Clover",
-    "category": "Mobile / Handheld",
-    "features": {
-      "Wi-Fi": "Yes",
-      "Cellular": "LTE"
-    },
-    "specs": {
-      "Wi-Fi": "Yes",
-      "Cellular": "LTE"
-    }
-  },
-  {
-    "image": "https://www.cardmachineoutlet.com/1_26f7b3c1-3f1a-45c1-8a23-3e2f7a9c1b23_grande.png",
-    "model": "Clover Mini Countertop",
-    "brand": "Clover",
-    "category": "Countertop"
-  },
-  {
-    "image": "https://www.cardmachineoutlet.com/1_21c4a2e1-3f11-4759-b42c-65e23f1bc9a0_grande.png",
-    "model": "Ingenico Move 3500 4G / Wi-Fi / Bluetooth",
-    "brand": "Ingenico",
-    "category": "Mobile / Handheld",
-    "features": {
-      "Wi-Fi": "Yes",
-      "Cellular": "4G",
-      "Bluetooth": "Yes"
-    },
-    "specs": {
-      "Wi-Fi": "Yes",
-      "Cellular": "4G",
-      "Bluetooth": "Yes"
-    }
-  }
-];
+import productsData from '../data/products-data.json';
 
 const HardwareSpotlight = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
-    if (hardwareData.length === 0) return;
+    if (productsData.length === 0) return;
     const timer = setTimeout(() => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % hardwareData.length);
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % productsData.length);
     }, 5000); // Rotate every 5 seconds
 
     return () => clearTimeout(timer);
   }, [currentIndex]);
 
-  if (hardwareData.length === 0) {
+  if (productsData.length === 0) {
     return <div className="text-center text-red-500">No hardware data to display.</div>;
   }
 
-  const currentHardware = hardwareData[currentIndex];
+  const currentHardware = productsData[currentIndex];
 
   if (!currentHardware) {
     return null;
@@ -423,33 +27,43 @@ const HardwareSpotlight = () => {
   return (
     <div className="relative bg-gray-900 text-white p-8 rounded-lg shadow-2xl overflow-hidden">
       <div className="flex flex-col md:flex-row gap-8">
-        <div className="md:w-1/2 flex items-center justify-center">
-          <Image
-            src={currentHardware.image}
-            alt={currentHardware.model}
-            width={400}
-            height={400}
-            className="object-contain rounded-lg"
-            unoptimized // Use this as a fallback if the default loader still causes issues
-          />
+        <div className="md:w-1/2 flex items-center justify-center bg-white/5 rounded-lg p-4">
+          <div className="relative w-full h-[300px]">
+            <Image
+              src={currentHardware.image}
+              alt={`${currentHardware.make} ${currentHardware.model}`}
+              fill
+              className="object-contain"
+              unoptimized
+            />
+          </div>
         </div>
         <div className="md:w-1/2 flex flex-col justify-center">
-          <h2 className="text-3xl font-bold mb-2">{currentHardware.brand}</h2>
+          <h2 className="text-3xl font-bold mb-2">{currentHardware.make}</h2>
           <h3 className="text-xl text-gray-400 mb-4">{currentHardware.model}</h3>
-          <p className="text-lg mb-6 bg-gray-800 px-4 py-2 rounded-md self-start">{currentHardware.category}</p>
           
-          {currentHardware.specs && Object.keys(currentHardware.specs).length > 0 && (
-            <div>
-              <h4 className="text-2xl font-semibold mb-3">Specifications</h4>
-              <div className="grid grid-cols-2 gap-x-6 gap-y-3">
-                {Object.entries(currentHardware.specs).map(([key, value]) => (
-                  <div key={key} className="flex justify-between border-b border-gray-700 py-1">
-                    <span className="font-medium text-gray-300">{key}:</span>
-                    <span className="text-gray-400">{String(value)}</span>
-                  </div>
-                ))}
-              </div>
-            </div>
+          <div className="mb-6">
+            <h4 className="text-lg font-semibold mb-2 text-gray-300">Description</h4>
+            <p className="text-gray-400 leading-relaxed">
+              {currentHardware.description}
+            </p>
+          </div>
+
+          {currentHardware.price && (
+             <div className="mt-auto">
+               <span className="text-2xl font-bold text-blue-400">{currentHardware.price}</span>
+             </div>
+          )}
+          
+          {currentHardware.productUrl && (
+            <a 
+              href={currentHardware.productUrl} 
+              target="_blank" 
+              rel="noopener noreferrer"
+              className="mt-4 inline-block bg-blue-600 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded transition-colors self-start"
+            >
+              View Details
+            </a>
           )}
         </div>
       </div>
